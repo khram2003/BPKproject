185d320f68c8da86cdb17681ab3ea3d2cb4fbea8
alexbuyan <sashabuyan@yandex.ru>
2021-03-01 02:01:08 +0300

    playing with database

--- server/server.cpp
+++ server/server.cpp
@@ -24,5 +24,6 @@ int main() {
     database.add_user(user2);
     database.add_chat(chat_name);
     database.add_message(1, sender_id, recipient_id, text);
+    //database.add_message(100, sender_id, recipient_id, text);
     return 0;
 }
