ce23a41805be2b5f686b5fa579ba763aa6f1e62e
Nikita <nikita.khramov@gmail.com>
2021-03-02 12:14:37 +0300

    added cmake for client

--- client/mainwindow.h
+++ client/mainwindow.h
@@ -7,15 +7,16 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow() override;
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
