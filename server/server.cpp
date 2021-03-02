c2a99839eff329b062bbd177c133a9c3ad5917fd
parseny <parseny@mail.ru>
2021-03-02 23:58:49 +0300

    temporary changes at server

--- server/server.cpp
+++ server/server.cpp
@@ -2,11 +2,8 @@
 #include <model.h>
 #include <functional>
 #include <iostream>
-#include "third-party/websocketpp-0.8.2"
-#pragma comment(lib, "ws2_32.lib")
+//#pragma comment(lib, "ws2_32.lib")
 #define ASIO_STANDALONE
-
-typedef websocketpp::server<websocketpp::config::asio> server;
 
 int main() {
     model::Message msg(1, 2, "hello");
@@ -23,7 +20,7 @@ int main() {
               << ' ' << date.day() << ' ' << time << ' ' << date.year()
               << std::endl;
     std::cout << "Server started" << std::endl;
-    
+
     WSAData wsaData;
     WORD DLLVersion = MAKEWORD(2, 1);
     if (WSAStartup(DLLVersion, &wsaData) != 0) {
@@ -32,21 +29,22 @@ int main() {
     }
     SOCKADDR_IN addr;  // хранение адреса
     auto sizeofaddr = sizeof(addr);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1") addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1234);
     addr.sin_family = AF_INET;
 
     SOCKET sListen;
-    sListen =
-        socket(AF_INET, SOCK_STREAM,
-               NULL);  // исп семейстов интеренет протовколов. 2-й указывает на
-                       // протокол, устанавливающий соединение
+    sListen = socket(AF_INET, SOCK_STREAM,
+                     0);  // исп семейстов интеренет протовколов. 2-й указывает
+                          // на протокол, устанавливающий соединение
     bind(sListen, (SOCKADDR *)&addr, sizeof(addr));
     listen(sListen, SOMAXCONN);
 
     SOCKET newConnection;
-    newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+    newConnection = accept(sListen, (SOCKADDR *)&addr,
+                           reinterpret_cast<int *>(&sizeofaddr));
 
-    if(newConnection == 0) {
+    if (newConnection == 0) {
         std::cout << "Error of connection" << std::endl;
     } else {
         std::cout << "Client connected" << std::endl;
