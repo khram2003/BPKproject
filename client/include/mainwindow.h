f9e48b0b46a8dd6ab2142f2cbc506cc185297e01
khram2003 <nikita.khramov@gmail.com>
2021-04-18 14:38:09 +0300

    CMakeLists.txt should be changed

---
+++ client/include/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QListWidgetItem>
+#include <QMainWindow>
+#include <QStringListModel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow() override;
+
+private slots:
+    void on_listWidget_2_itemClicked(QListWidgetItem *item);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif  // MAINWINDOW_H
---
+++ client/src/main.cpp
@@ -0,0 +1,30 @@
+#include <QApplication>
+#include <QStyleFactory>
+#include "../include/mainwindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+    QPalette darkPalette;
+
+    // Настраиваем палитру для цветовых ролей элементов интерфейса
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    QApplication::setPalette(darkPalette);
+    MainWindow w;
+    w.show();
+    return QApplication::exec();
+}
