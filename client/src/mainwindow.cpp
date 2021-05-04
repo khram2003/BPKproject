d10f34b6080be380a79c3bb900474e11c87578f6
khram2003 <nikita.khramov@gmail.com>
2021-05-04 17:57:45 +0400

    Changed CMakeLists.txt and fixed paths

---
+++ client/src/mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include <QBrush>
+#include <QColor>
+#include <QFont>
+#include <QListWidgetItem>
+#include <QMainWindow>
+#include <QPixmap>
+#include <QPushButton>
+#include <QShortcut>
+#include "chat_view.h"
+#include "message_view.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    std::vector<QString> chats = {
+        "Biba", "Boba", "Bobo"};  // just for example, should be downloaded
+    for (auto x : chats) {
+        QListWidgetItem *item = new QListWidgetItem;
+        ChatView *row = new ChatView(x);
+        ui->listWidget_2->setWordWrap(true);
+        ui->listWidget_2->addItem(item);
+        ui->listWidget_2->setItemWidget(item, row);
+        item->setSizeHint(row->minimumSizeHint());
+        item->setFont(QFont("Helvetica [Cronyx]", 12));
+        item->setSizeHint(QSize(2, 52));
+    }
+
+    connect(ui->sendButton, &QPushButton::clicked, [this] {
+        if ((ui->textEdit->toPlainText()).size() != 0) {
+            ui->textEdit->setWordWrapMode(
+                QTextOption::WrapAtWordBoundaryOrAnywhere);
+            QListWidgetItem *item = new QListWidgetItem;
+            MessageViewIn *row = new MessageViewIn(ui->textEdit->toPlainText());
+            row->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+            ui->listWidget->setSizeAdjustPolicy(QListWidget::AdjustToContents);
+            ui->listWidget->setWordWrap(true);
+            ui->listWidget->addItem(item);
+            ui->listWidget->setItemWidget(item, row);
+            item->setSizeHint(row->sizeHint());
+            item->setFont(QFont("Helvetica [Cronyx]", 12));
+            ui->textEdit->setPlainText("");
+        }
+    });
+
+    connect(ui->listWidget_2, SIGNAL(itemClicked(QListWidgetItem *)),
+            ui->listWidget,
+            SLOT(on_listWidget_2_itemClicked(QListWidgetItem *)));
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::on_listWidget_2_itemClicked(QListWidgetItem *item) {
+    ui->listWidget->clear();
+}
