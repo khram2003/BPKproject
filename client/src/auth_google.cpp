43d4b3d719fc06a0c5c937ed5501c94ebede83ae
Arsenii Pimenov <parseny@mail.ru>
2021-05-27 23:56:59 +0300

    local google auth

--- client/src/auth_google.cpp
+++ client/src/auth_google.cpp
@@ -1,91 +1,48 @@
-#include "auth_google.h"
-#include <QDesktopServices>
-#include <QDir>
-#include <QFile>
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QOAuthHttpServerReplyHandler>
-#include <QObject>
-#include <QString>
-#include <QUrl>
 #include <iostream>
+#include <QDesktopServices>
+#include <QGuiApplication>
+#include <QObject>
+#include <QOAuth2AuthorizationCodeFlow>
+#include <QOAuthHttpServerReplyHandler>
 
-GoogleAuth::GoogleAuth(QObject *parent) : QObject(parent) {
-    this->google = new QOAuth2AuthorizationCodeFlow(this);
-    this->google->setScope("email");
+class GoogleAuth : public QObject {
+Q_OBJECT
+public:
+    GoogleAuth() {}
 
-    connect(this->google, &QOAuth2AuthorizationCodeFlow::authorizeWithBrowser,
-            &QDesktopServices::openUrl);
+    void work() {
+        QOAuth2AuthorizationCodeFlow google{this};
+        google.setScope("email");
+        connect(&google, &QOAuth2AuthorizationCodeFlow::authorizeWithBrowser,
+                &QDesktopServices::openUrl);
 
-    QString val;
-    QFile file;
-    // видимо надо указать свой путь до этого файла
-    //    file.setFileName(QDir::toNativeSeparators("secret.json"));
-    //    QDir::setCurrent(qApp->applicationDirPath());
-    //    file.setFileName(
-    //        "client_secret_656689579627_iulsp1abjnrtd8hhui64i27qq5etgaej_apps.json");
-    //    if (!file.exists()){
-    //        std::cerr<<file.fileName();
-    //    }
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        val = file.readAll();
-        file.close();
+        google.setAuthorizationUrl(
+            QUrl("https://accounts.google.com/o/oauth2/auth"));
+        google.setClientIdentifier(
+            "656689579627-iulsp1abjnrtd8hhui64i27qq5etgaej.apps.googleusercontent."
+            "com");
+        google.setAccessTokenUrl(
+            QUrl("https://oauth2.googleapis.com/token"));
+        google.setClientIdentifierSharedKey("NKhwrtNDtewe1dyswLVu-srR");
+
+
+        auto replyHandler = new QOAuthHttpServerReplyHandler(10002, this);
+        google.setReplyHandler(replyHandler);
+        connect(&google, &QOAuth2AuthorizationCodeFlow::granted, []() {
+          std::cerr << "Granted" << std::endl;
+        });
+        google.setModifyParametersFunction([](QAbstractOAuth::Stage stage, QMultiMap<QString, QVariant>* parameters) {
+          // Percent-decode the "code" parameter so Google can match it
+          if (stage == QAbstractOAuth::Stage::RequestingAccessToken) {
+              QByteArray code = parameters->value("code").toByteArray();
+              parameters->remove("code");
+              parameters->insert("code", QUrl::fromPercentEncoding(code));
+          }
+        });
+
+        QEventLoop eventLoop{this};
+        QObject::connect(&google, &QOAuth2AuthorizationCodeFlow::granted, &eventLoop, &QEventLoop::quit);
+        google.grant();
+        eventLoop.exec();
     }
-
-    QJsonDocument document = QJsonDocument::fromJson(val.toUtf8());
-    QJsonObject object = document.object();
-    const auto settingsObject = object["web"].toObject();
-
-    //    const QUrl authUri(settingsObject["auth_uri"].toString());
-    const QUrl authUri("https://accounts.google.com/o/oauth2/auth");
-
-    //    const auto clientId = settingsObject["client_id"].toString();
-    const QString clientId =
-        "656689579627-iulsp1abjnrtd8hhui64i27qq5etgaej.apps.googleusercontent."
-        "com";
-
-    //    const QUrl tokenUri(settingsObject["token_uri"].toString());
-    const QUrl tokenUri("https://oauth2.googleapis.com/token");
-
-    //    const auto clientSecret(settingsObject["client_secret"].toString());
-    const auto clientSecret("NKhwrtNDtewe1dyswLVu-srR");
-    //    const auto redirectUris = settingsObject["redirect_uris"].toArray();
-    [[maybe_unused]] const auto redirectUris = "http://127.0.0.1:9008/";
-    //    const QUrl redirectUri(redirectUris[0].toString());
-    const QUrl redirectUri("http://127.0.0.1:9008/");
-    const auto port = static_cast<quint16>(9008);
-
-    this->google->setAuthorizationUrl(authUri);
-    this->google->setClientIdentifier(clientId);
-    this->google->setAccessTokenUrl(tokenUri);
-    this->google->setClientIdentifierSharedKey(clientSecret);
-
-    //    this->google->setModifyParametersFunction([&](QAbstractOAuth::Stage
-    //    stage, QVariantMap* parameters) {
-    //      // Percent-decode the "code" parameter so Google can match it
-    //      if (stage == QAbstractOAuth::Stage::RequestingAccessToken) {
-    //          QByteArray code = parameters->value("code").toByteArray();
-    //          (*parameters)["code"] = QUrl::fromPercentEncoding(code);
-    //      }
-    //    });
-
-    auto replyHandler = new QOAuthHttpServerReplyHandler(port, this);
-    this->google->setReplyHandler(replyHandler);
-    this->google->grant();
-
-    connect(this->google, &QOAuth2AuthorizationCodeFlow::granted, [=]() {
-        const QString token = this->google->token();
-        emit gotToken(token);
-    });
-
-    //    connect(this->google, &QOAuth2AuthorizationCodeFlow::granted, [=]() {
-    //        qDebug() << __FUNCTION__ << __LINE__ << "Access Granted!";
-    //        auto reply =
-    //        this->google->get(QUrl("https://www.googleapis.com/plus/v1/people/me"));
-    //        connect(reply, &QNetworkReply::finished, [reply]() {
-    //            qDebug() << "REQUEST FINISHED. Error? " << (reply->error() !=
-    //            QNetworkReply::NoError); qDebug() << reply->readAll();
-    //        });
-    //    });
-}
+};
